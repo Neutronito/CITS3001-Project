5ed7d4075d36e2811215c60dd8d7c83815afa6c8
neutronito <vg.sinalco@gmail.com>
2022-08-27 16:29:31 +0800

    Implemented copying jgraph graph to an array adjancency matrix and created a green network print function

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import org.jgrapht.alg.matching.GreedyMaximumCardinalityMatching;
 import org.jgrapht.generate.*;
 import java.util.function.Supplier;
 import org.jgrapht.graph.DefaultWeightedEdge;
@@ -5,7 +6,8 @@ import org.jgrapht.graph.SimpleWeightedGraph;
 
 public class Game {
 
-    private SimpleWeightedGraph<Integer, DefaultWeightedEdge> greenNetwork;
+    private double[][] greenNetwork;
+    private int greenNodeCount = 0;
 
     /**
      * Creates an instance of the game using the input parameters
@@ -16,12 +18,14 @@ public class Game {
      * @param greenVotePercent The proportion of green nodes who wish to vote (at the start of the game) as a percentage
      */
     public Game(int greenNodeCount, double probabilityOfConnection, int greyCount, double greyEvilProportion, double greenVotePercent) {
+        //Fill variables
+        this.greenNodeCount = greenNodeCount;
 
         //Use the jgraph library to create a graph generator that uses Erdos Renyi Model
         GnpRandomGraphGenerator<Integer,DefaultWeightedEdge> greenNetworkGenerator = new GnpRandomGraphGenerator<>(greenNodeCount, probabilityOfConnection);
         
         //Init the graph 
-        greenNetwork = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+        SimpleWeightedGraph<Integer, DefaultWeightedEdge> greenNetworkjgrapht = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
         
         //Create the vertex supplier
         Supplier<Integer> greenSupplier = new Supplier<Integer>() {
@@ -32,21 +36,54 @@ public class Game {
             }
         };
 
-        greenNetwork.setVertexSupplier(greenSupplier);
+        greenNetworkjgrapht.setVertexSupplier(greenSupplier);
 
         //Use the generator to make the graph
-        greenNetworkGenerator.generateGraph(greenNetwork);
+        greenNetworkGenerator.generateGraph(greenNetworkjgrapht);
 
-        //Debugging lemme have a look through this graph
+        //Now put the weights into the matrix
+        greenNetwork = new double[greenNodeCount][greenNodeCount];
+
         for (int i = 0; i < greenNodeCount; i++) {
             for (int j = 0; j < greenNodeCount; j++) {
-                //System.out.println("We are currently looking at path (" + i + ", " + j + ") and the weight is " + greenNetwork.getEdgeWeight(greenNetwork.getEdge(i, j)));
+                //Check if it exists
+                if (i == j) {
+                    greenNetwork[i][j] = 1.0;
+                    continue;
+                }
+                if (greenNetworkjgrapht.getEdge(i, j) != null) {
+                    greenNetwork[i][j] = greenNetworkjgrapht.getEdgeWeight(greenNetworkjgrapht.getEdge(i, j));
+                } else {
+                    greenNetwork[i][j] = 0;
+                }
             }
+        }
+    }
+
+    /**
+     * Print out the green network as an adjacency matrix with all the edge weights
+     */
+    public void printGreenNetwork() {
+
+        System.out.print("\t");
+        for (int i = 0; i < greenNodeCount; i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+
+
+        for (int i = 0; i < greenNodeCount; i++) {
+            System.out.print(i + "\t");
+            for (int j = 0; j < greenNodeCount; j++) {
+                System.out.print(greenNetwork[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
         Game curGame = new Game(10, 0.4, 0, 0.0, 10.0);
+        curGame.printGreenNetwork();
     }
 
 }
