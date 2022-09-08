ced9e8118ffcca00f4b92a7e4673cef738c81582
neutronito <vg.sinalco@gmail.com>
2022-09-08 12:25:44 +0800

    Implemented a game runner function that accepts terminal input each turn

--- GameRunner.java
+++ GameRunner.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GameRunner {
 
     private Game gameInstance;
@@ -18,23 +20,66 @@ public class GameRunner {
 
     /**
      * Returns the game instance. It returns a shallow copy so TAKE CARE.
-     * @return
+     * @return A shallow copy of the game instance, of the type Game class
      */
     public Game getGameInstance() {
         return gameInstance;
     }
 
+    /**
+     * A blocking function that executes the game, waiting for human inputs for the red turn. The red turn is executed before the green turn
+     * Prints the game statistics after each turn.
+     * The game ends once the player passes in the end game command.
+     * @return 0 on game end, anything else denotes an error.
+     */
+    public int playGame() {
+        
+        gameInstance.printGreenAgents();
+        gameInstance.printGreenStatistics();
+
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("\n \n \n Choose the red level of potency, from 1 to 6");
+            //Wait for an input from the user
+            String userInput = scanner.nextLine();
+            
+            //Error check and parse the input
+            if (userInput.length() != 1) {
+                System.out.println("Error, please enter a single digit.");
+                continue;
+            }
+
+            int redPotency = Integer.parseInt(userInput);
+
+            //Check for escape integer
+            if (redPotency == 0) {
+                break;
+            }
+
+            if (redPotency < 1 || redPotency > 6) {
+                System.out.println("Error, please enter a digit from 1 to 6, inclusively");
+                continue;
+            }
+
+            //Execute the red turn
+            gameInstance.executeRedTurn(redPotency);
+
+            //Execute the green turn
+            gameInstance.executeGreenTurn();
+
+            //Print the metrics now
+            gameInstance.printGreenAgents();
+            gameInstance.printGreenStatistics();
+        }
+        scanner.close();
+        return 0;
+    }
+
     public static void main(String[] args) {
         double[] uncertaintyInterval = {-1.0, 0.4};
-        GameRunner curRunner = new GameRunner(15, 0.4, 10, 40.0, uncertaintyInterval, 60.0);
-        Game curGame = curRunner.getGameInstance();
-        curGame.printGreenAgents();
-        curGame.printGreenStatistics();
-        for (int i = 0; i < 10; i++) {
-            curGame.executeGreenTurn();
-        }
-        System.out.println();
-        curGame.printGreenAgents();
-        curGame.printGreenStatistics();
+        GameRunner curRunner = new GameRunner(40, 0.4, 10, 40.0, uncertaintyInterval, 60.0);
+        curRunner.playGame();
+        System.out.println("The game is over");
     }
 }
