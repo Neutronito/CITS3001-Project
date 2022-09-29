cffe2a2013bc1b6246fc58f7a0f7017fa48a4c8f
neutronito <vg.sinalco@gmail.com>
2022-09-29 17:46:28 +0800

    Created and implemented class to graph green opinions over time

---
+++ OpinionGrapher.py
@@ -0,0 +1,41 @@
+from ast import arg
+import numpy as np 
+import matplotlib.pyplot as plt 
+import sys
+  
+
+
+# process cmd line arguments
+# Syntax is: Each round is split by a | while inside the two numbers are split by a ,
+
+redCount = []
+blueCount = []
+
+argumentsList = sys.argv[1].split("_")
+
+for argument in argumentsList:
+    values = argument.split(",")
+
+    print("First value is " + values[0])
+    print("Second value is " + values[1])
+    redCount.append(int(values[0]))
+    blueCount.append(int(values[1]))
+
+
+
+print(redCount)
+print(blueCount)
+  
+X = list(range(0, len(redCount)))
+  
+X_axis = np.arange(len(X))
+  
+plt.bar(X_axis - 0.2, blueCount, 0.4, label = 'Blue Team')
+plt.bar(X_axis + 0.2, redCount, 0.4, label = 'Red Team')
+  
+plt.xticks(X_axis, X)
+plt.xlabel("Groups")
+plt.ylabel("Number of Students")
+plt.title("Number of Students in each group")
+plt.legend()
+plt.show()
