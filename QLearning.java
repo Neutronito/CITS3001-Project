6fae36e65b2a67202d7169d421ffb66fcbd38baa
LisonLau <101380095+LisonLau@users.noreply.github.com>
2022-10-11 13:45:26 +0800

    Added final state stuff

--- QLearning.java
+++ QLearning.java
@@ -44,17 +44,44 @@ public class QLearning {
         }
     }
 
-    public boolean isFinalState(int[] votingOpinions) {
-        // If AI is blue, desired state is voters > non-voters
+    public boolean isFinalState(int[] votingOpinions, boolean triggerGameEnd, int numIterations) {
+        // If AI is blue, desired state is voters > non-voters when game has ended
         if (isBlue) {
-            return votingOpinions[1] > votingOpinions[0];
+            return (votingOpinions[1] > votingOpinions[0] && (triggerGameEnd || numIterations == 40));
         } 
         // If AI is red, desired state is non-voters > voters
         else {
-            return votingOpinions[0] > votingOpinions[1];
+            return (votingOpinions[0] > votingOpinions[1] && (triggerGameEnd ||  numIterations == 40));
         }
     }
          
 }
 
 
+
+// void calculateQ() {
+//     Random rand = new Random();
+
+//     for (int i = 0; i < 1000; i++) { // Train cycles
+//         // Select random initial state
+//         int crtState = rand.nextInt(statesCount);
+
+//         while (!isFinalState(crtState)) {
+//             int[] actionsFromCurrentState = possibleActionsFromState(crtState);
+
+//             // Pick a random action from the ones possible
+//             int index = rand.nextInt(actionsFromCurrentState.length);
+//             int nextState = actionsFromCurrentState[index];
+
+//             // Q(state,action)= Q(state,action) + alpha * (R(state,action) + gamma * Max(next state, all actions) - Q(state,action))
+//             double q = Q[crtState][nextState];
+//             double maxQ = maxQ(nextState);
+//             int r = R[crtState][nextState];
+
+//             double value = q + alpha * (r + gamma * maxQ - q);
+//             Q[crtState][nextState] = value;
+
+//             crtState = nextState;
+//         }
+//     }
+// }
